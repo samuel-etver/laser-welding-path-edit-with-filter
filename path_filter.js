8cec5b1489d60d5dae29114e28adb6a973d3e344
samuel-etver <samuel.etver@gmail.com>
2020-03-08 19:12:56 +0500

    Added filter

---
+++ path_filter.js
@@ -0,0 +1,243 @@
+
+function filterPath(filterName, data, options) {
+  if ( !filterName ) {
+    filterName = 'default';
+  }
+
+  switch(filterName) {        
+    case 'default':
+    case 'diff': 
+      return filterPathWithDiff(data, options);            
+    case 'med':
+      return filterPathWithMed(data, options);            
+  }
+}
+
+
+function filterPathWithDiff(data, options) {
+  var deltaYmax = 0.1;
+  if ( !options || !options.deltaYmax ) {
+    deltaYmax = options.deltaYmax;
+  }
+
+  var arrIn = data.arrIn;
+  var arrStatus = data.arrStatus;
+
+  var arrXY = filterUtils.buildArrayXY(arrIn, arrStatus);
+  if ( arrXY.length == 0 ) {
+    return undefined;
+  }
+  if ( arrXY.length == 1 ) {
+    return filterUtils.interpolate(arrIn, arrStatus);
+  }
+
+  var factors = filterUtils.calcFactors3( arrXY );
+  var calcY = x => filterUtils.calcPolinom3(factors, x);
+  var n = arrIn.length;
+
+  var y0 = arrStatus[0] ? arrIn[0] : calcY(0);
+  var y1;
+  var arrOut = [y0, ];
+  for (var i = 1; i < n; i++) {
+    if ( arrStatus[i] ) {
+      y1 = arrIn[i];
+    }
+    else {
+      y1 = y0 + (calcY(i) - calcY(i - 1));
+    }
+    y0 = y1;
+  }
+
+  return filterUtils.interpolate(arrOut, arrStatus);
+}
+
+function filterPathWithMed(data, options) {
+}
+
+
+var filterUtils = {
+  getTrueCount: function (arrIn) {
+    var count = 0;
+    arrIn && arrIn.forEach(value => {
+      if ( value ) {
+        count++;
+      }
+    });
+    return count;
+  },
+
+  calcDeterminant3: function(a) {
+    return  a[0][0]*a[1][1]*a[2][2] + 
+            a[0][1]*a[1][2]*a[2][0] + 
+            a[0][2]*a[1][0]*a[2][1] - 
+            a[0][0]*a[1][2]*a[2][1] - 
+            a[0][1]*a[1][0]*a[2][2] - 
+            a[0][2]*a[1][1]*a[2][0];
+  },
+  
+  solveLinear3: function(arrIn) {
+    var n = 3;
+    var desI = [0, 0, 0];
+    var matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+    
+    for (var i = 0; i < n; i++) {
+      matrix[i][0] = arrIn[i][0];
+      matrix[i][1] = arrIn[i][1];
+      matrix[i][2] = arrIn[i][2];
+    }
+    var des = this.calcDeterminant3(matrix);
+    if ( !(des) ) {
+      return null;
+    }              
+
+    for (i = 0; i < n; i++) {
+      matrix[i][0] = arrIn[i][3];
+      matrix[i][1] = arrIn[i][1];
+      matrix[i][2] = arrIn[i][2];
+    }
+    desI[0] = this.calcDeterminant3(matrix);
+
+
+    for (i = 0; i < n; i++) {
+      matrix[i][0] = arrIn[i][0];
+      matrix[i][1] = arrIn[i][3];
+      matrix[i][2] = arrIn[i][2];
+    }
+    desI[1] = this.calcDeterminant3(matrix);
+
+
+    for (i = 0; i < n; i++) {
+      matrix[i][0] = arrIn[i][0];
+      matrix[i][1] = arrIn[i][1];
+      matrix[i][2] = arrIn[i][3];
+    }
+    desI[2] = this.calcDeterminant3(matrix);
+
+
+    var aout = [];
+    for (i = 0; i < 3; i++) {
+      aout.push( desI[i]/des );
+    }
+    return aout;
+  },
+  
+  /*
+    * Method of least squeares
+    */
+  calcFactors3: function(arrIn) {
+    var n = arrIn.length; 
+
+    var x;
+    var y;
+    var mx = 0.0;
+    var my = 0.0;
+    var mxy = 0.0;
+    var mx2y = 0.0;
+    var mx2 = 0.0;
+    var mx3 = 0.0;
+    var mx4 = 0.0;
+
+    for (var i = 0; i < n; i++) {
+      x = arrIn[i][0];
+      y = arrIn[i][1];
+      mx += x;
+      my += y;
+      mxy += x*y;
+      x2 = x*x;
+      mx2y += x2*y;
+      mx2 += x2;
+      mx3 += x2*x;
+      mx4 += x2*x2;
+    }
+
+    if ( n ) {
+      return this.solveLinear3([[mx4, mx3, mx2, mx2y],
+                                [mx3, mx2, mx,  mxy], 
+                                [mx2, mx,  n,   my]]);
+    }
+
+    return null;
+  },
+  
+  calcPolinom3: function(factors, arg) {
+    return  factors[0]*arg*arg +
+            factors[1]*arg +
+            factors[2];
+  },
+  
+  buildArrayXY: function(arrIn, arrStatus) {
+    var arrOut = [];
+    var n = arrIn.length;
+    for (var i = 0; i < n; i++) {
+      if ( !arrStatus || arrStatus[i]) {
+        arrOut.push( [i, arrIn[i]] );
+      }
+    }
+    return arrOut;
+  },
+  
+  makeGood: function(arrStatus) {
+    var n = arrStatus.length;
+    for (var i = 0; i < n; i++) {
+      arrStatus[i] = 1;
+    }
+  },
+  
+  interpolate: function(arrIn, arrStatus) {
+    var n = arrIn.length;
+    var arrOut = [];
+    for (var i = 0; i < n; i++) {
+      arrOut.push( arrIn[i] );
+    }
+
+    var index0;
+      
+    var doInterpolation = function(loIndex, hiIndex) {
+      if ( loIndex === undefined) {
+        var value = arrOut[hiIndex];
+        for (var i = 0; i < hiIndex; i++) {
+          arrOut[i] = value;
+        }
+      } 
+      else {
+        var y0 = arrOut[loIndex];
+        var y1 = arrOut[hiIndex];
+        for (var i = loIndex + 1; i < hiIndex; i++) {
+          arrOut[i] = y0 + (i - loIndex)*(y1 - y0)/(hiIndex - loIndex);
+        }
+      }
+    }
+      
+    for (var i = 0; i < n; i++) {
+      if ( !arrStatus[i] ) {
+        continue;
+      }            
+      if (i - 1 !== index0) {
+        doInterpolation(index0, i);
+      }            
+      index0 = i;            
+    }
+      
+    if ( index0 != i ) {
+      if ( index0 === undefined) {
+        for (i = 0; i < n; i++) {
+          arrOut[i] = 0;
+        }
+      }
+      else {
+        var value = arrOut[index0];
+        for (i = index0 + 1; i < n; i++) {
+          arrOut[i] = value;
+        }
+      }
+    }
+
+    return arrOut;
+  }, 
+}
+
+
+module.exports = {
+  filterPath: filterPath,
+  utils: filterUtils,
+}
---
+++ test/test_filters.js
@@ -0,0 +1,155 @@
+
+const chai = require('chai')
+const mocha = require('mocha')
+
+const expect = chai.expect;
+
+const filters = require('../path_filter.js')
+
+const filterUtils = filters.utils;
+
+
+describe('Calculating Determinant Test', () => {
+    let calcDeterminant3 = (arg) => filterUtils.calcDeterminant3(arg);
+    it('Calculating Determinant', () => {
+        let value = calcDeterminant3([[2,3,1], [5, 8, 3], [-2, 0, 5]]);
+        expect(value).to.equal(3);
+    });
+    it('Calulcating Determinant', () => {
+        let value = calcDeterminant3([[3, 3, -1], [4, 1, 3], [1, -2, -2]]);
+        expect(value).to.equal(54);
+    });
+    it('Calulcating Determinant', () => {
+        let value = calcDeterminant3([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+        expect(value).to.equal(0);
+    });
+});
+
+describe('Linear Equation Test', () => {
+    let solveLinear3 = arg => filterUtils.solveLinear3(arg);
+    it('Solve Linear Equation', () => {
+        let result = solveLinear3([[2, 3, -1, 9], [1, -2, 1, 3], [1, 0, 2, 2]]);
+        expect(result[0]).to.be.closeTo( 4.0, 1e-7);
+        expect(result[1]).to.be.closeTo( 0.0, 1e-7);
+        expect(result[2]).to.be.closeTo(-1.0, 1e-7);
+    });
+    it('Solve Linear Equation', () => {
+        let result = solveLinear3([[1, 2, 3, 6], [4, 5, 6, 9], [7, 8, 0, -6]]);
+        expect(result[0]).to.be.closeTo(-2.0, 1e-7);
+        expect(result[1]).to.be.closeTo( 1.0, 1e-7);
+        expect(result[2]).to.be.closeTo( 2.0, 1e-7);
+    });
+});
+
+describe('Least Squares Method Test', () => {
+    let calcFactors3 = arg => filterUtils.calcFactors3(arg);
+    let buildArrayXY = (arg1, arg2) => filterUtils.buildArrayXY(arg1, arg2);
+    let calc = (arg1, arg2) => calcFactors3(buildArrayXY(arg1, arg2));
+    let errVal = -1000;
+    let buildStatuses = function(ain) {
+        let aout = [];
+        ain.forEach(a => aout.push(a == errVal ? 0 : 1));
+        return aout;
+    }
+    
+    it('Using Least Squares Method', () => {
+        let dots     = [0, 1, 2, errVal, errVal, 5, 6, errVal];
+        let statuses = buildStatuses( dots );
+        let result = calc(dots, statuses);
+        expect(result[0]).to.be.closeTo(0.0, 1e-7);
+        expect(result[1]).to.be.closeTo(1.0, 1e-7);
+        expect(result[2]).to.be.closeTo(0.0, 1e-7);
+    });
+    
+    it('Using Least Squares Method', () => {
+        let dots     = [0.1, 0.1, 0.2, 0.2, 0.0, 0.0];
+        let statuses = buildStatuses( dots );
+        let result = calc(dots, statuses);
+        expect(result[0]).to.be.closeTo( -0.02142857, 1e-7);
+        expect(result[1]).to.be.closeTo(  0.08428571, 1e-7);
+        expect(result[2]).to.be.closeTo(  0.08571428, 1e-7);
+    });
+    
+    it('Using Least Squares Method', () => {
+        let dots     = [0.1, 0.1, 0.2, 0.2, 0.0, 0.0, errVal];
+        let statuses = buildStatuses( dots );
+        let result = calc(dots, statuses);
+        expect(result[0]).to.be.closeTo( -0.02142857, 1e-7);
+        expect(result[1]).to.be.closeTo(  0.08428571, 1e-7);
+        expect(result[2]).to.be.closeTo(  0.08571428, 1e-7);
+    });
+});
+
+describe('Polinom Calcuation Test', () => {
+    let calc = (factors, x) => filterUtils.calcPolinom3(factors, x);
+
+    it('Calculating polinom', () => {
+        let result = calc([1.0, 2.0, 3.0], 2.0);
+        expect(result).to.be.closeTo(11, 1e-7);
+    });
+
+    it('Calculating polinom', () => {
+        let result = calc([-0.9, 0.5, 0.13], 0.83);
+        expect(result).to.be.closeTo(-0.075010, 1e-7);
+    });
+});
+
+describe('Interpolation Test', () => {
+    let errVal = -1000;
+    let val = 10;
+    
+    it('Interpolating data', () => {   
+        let arrIn = [errVal, errVal, errVal, val];
+        let arrStatus = [0, 0, 0, 1];
+        var result = filterUtils.interpolate(arrIn, arrStatus);
+        expect(result[0]).to.be.closeTo(val, 1e-7);
+        expect(result[1]).to.be.closeTo(val, 1e-7);
+        expect(result[2]).to.be.closeTo(val, 1e-7);
+        expect(result[3]).to.be.closeTo(val, 1e-7);
+    });
+    
+    it('Interpolating data', () => {   
+        let arrIn = [val, errVal, errVal, errVal];
+        let arrStatus = [1, 0, 0, 0];
+        var result = filterUtils.interpolate(arrIn, arrStatus);
+        expect(result[0]).to.be.closeTo(val, 1e-7);
+        expect(result[1]).to.be.closeTo(val, 1e-7);
+        expect(result[2]).to.be.closeTo(val, 1e-7);
+        expect(result[3]).to.be.closeTo(val, 1e-7);
+    });
+    
+    it('Interpolating data', () => {   
+        let arrIn = [errVal, errVal, errVal, errVal];
+        let arrStatus = [0, 0, 0, 0];
+        var result = filterUtils.interpolate(arrIn, arrStatus);
+        expect(result[0]).to.be.closeTo(0, 1e-7);
+        expect(result[1]).to.be.closeTo(0, 1e-7);
+        expect(result[2]).to.be.closeTo(0, 1e-7);
+        expect(result[3]).to.be.closeTo(0, 1e-7);
+    });
+
+    it('Interpolating data', () => {   
+        let arrIn = [0, errVal, errVal, 3];
+        let arrStatus = [1, 0, 0, 1];
+        var result = filterUtils.interpolate(arrIn, arrStatus);
+        expect(result[0]).to.be.closeTo(0, 1e-7);
+        expect(result[1]).to.be.closeTo(1, 1e-7);
+        expect(result[2]).to.be.closeTo(2, 1e-7);
+        expect(result[3]).to.be.closeTo(3, 1e-7);
+    });
+});
+
+describe('Diff Filter Test', () => {
+    it('Testing filter. Not checking signal filtering quality!', () => {
+        var arrIn = [0, 0, 0, 1, 0];
+        var arrStatus = [1, 1, 1, 1, 1];
+        var inData  = {
+            arrIn: arrIn,
+            arrStatus: arrStatus
+        };
+        var result = filters.filterPath('diff', inData, {
+            deltaXmax: 0.1
+        });
+        console.log(result);
+    });
+});
